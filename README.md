3582f48972bef4351f7ce222872cd95da6d8f68b
Kate Edmundson <kate@kateedmundson.com>
2015-02-22 22:56:33 -0500

    sketch out the bones, add to do items

---
+++ README.md
@@ -0,0 +1,14 @@
+To do
+
+model:
+- get data from nycservice, volunteermatch or allforgood
+- save current location
+- save last search parameters to local storage?
+
+view:
+- Find volunteer jobs in [zip] or Get my location
+- display for this week, option to skip to next week
+- narrow by keyword
+- narrow by -keyword [NOT]
+
+
---
+++ js/app.js
@@ -0,0 +1,34 @@
+(function () {
+
+  var model = {
+    baseURL: 'http://www.volunteermatch.org/api/call?action=helloWorld'
+  };
+  
+  var controller = {
+    init: function() {
+      view.init();
+    },
+
+    getData: function() {
+      // add &query parameter to concatenate to baseURL
+      // build api call:
+      // use $.ajax beforeSend to add HTTP header with auth info
+      $.getJSON(model.baseURL, function(data) {
+      });
+    },
+
+    showResults: function() {}
+
+  };
+
+  var view = {
+    init: function() {
+      var $results = $('#results');
+      // controller.getData();
+    }
+
+  };
+
+  controller.init();
+
+})();
