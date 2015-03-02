b103e46e2a58902b3bb84923bea6192560fbe30c
Kate Edmundson <kate@kateedmundson.com>
2015-03-02 16:06:27 -0500

    get geolocation

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@
 (function () {
 
   var model = {
+
     wifiURL: 'https://nycopendata.socrata.com/api/views/jd4g-ks2z/rows.json'
+
   };
   
   var controller = {
+
     init: function() {
       view.init();
       this.getWifiData();
@@ -14,21 +17,48 @@
 
     getWifiData: function() {
       $.getJSON(model.wifiURL, function(result) {
-        console.log(result.data.length);
         for (var i = 0; i < result.data.length; i++) {
-          console.log('name of spot is: ' + result.data[i][12]);
+          // Spot name: result.data[i][12] 
+          // Lat: result.data[i][14]
+          // Long: result.data[i][15]
         }
       });
 
     },
 
-    showResults: function() {}
+    showResults: function() {},
+
+    getGeolocation: function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(view.showLatLong);
+      } else {
+        view.error("Geolocation is not supported by your browser");
+      }
+
+    }
 
   };
 
   var view = {
+
     init: function() {
-      var $results = $('#results');
+      this.$results = $('#results');
+      this.$error = $('#error');
+      this.$findBtn = $('#find');
+      this.$map = $('#map');
+
+      this.$findBtn.click(function() {
+        controller.getGeolocation();
+      });
+    },
+
+    error: function(message) {
+      this.$error.html(message);
+    },
+
+    showLatLong: function(position) {
+      view.$results.html('Latitude: ' + position.coords.latitude +
+                    '<br /> Longitude: ' + position.coords.longitude);
     }
 
   };
