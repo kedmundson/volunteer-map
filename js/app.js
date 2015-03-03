2206638cbb155b89dac127acc9e2ab474f39bbe7
Kate Edmundson <kate@kateedmundson.com>
2015-03-03 12:11:59 -0500

    model init, save wifi json to local storage

--- js/app.js
+++ js/app.js
@@ -4,37 +4,74 @@
 
   var model = {
 
-    wifiURL: 'https://nycopendata.socrata.com/api/views/jd4g-ks2z/rows.json'
+    wifiURL: 'https://nycopendata.socrata.com/api/views/jd4g-ks2z/rows.json',
+
+    init: function() {
+      if (!localStorage.wifiData) {
+        this.getWifiJSON();
+      } else {
+        console.log('localStorage.wifiData is ready.');
+      }
+    },
+
+    getWifiJSON: function() {
+      $.ajax({ url: this.wifiURL })
+            .done(function(result) {
+              // Save result to local storage
+              localStorage.wifiData = JSON.stringify(result.data);
+              console.log('Saved wifiData to localStorage.');
+            });
+    },
+
+    getPoints: function() {
+      return JSON.parse(localStorage.wifiData);
+
+      // Spot name: result.data[i][12] 
+      // Lat: result.data[i][14]
+      // Long: result.data[i][15]
+    }
+
 
   };
   
   var controller = {
 
     init: function() {
+      model.init();
       view.init();
-      this.getWifiData();
-    },
-
-    getWifiData: function() {
-      $.getJSON(model.wifiURL, function(result) {
-        for (var i = 0; i < result.data.length; i++) {
-          // Spot name: result.data[i][12] 
-          // Lat: result.data[i][14]
-          // Long: result.data[i][15]
-        }
-      });
-
     },
 
     showResults: function() {},
 
     getGeolocation: function() {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(view.showLatLong);
+        navigator.geolocation.getCurrentPosition(controller.delegateGeoInfo);
       } else {
         view.error("Geolocation is not supported by your browser");
       }
+    },
 
+    delegateGeoInfo: function(position) {
+      view.showLatLong(position);
+      controller.getClosestPoints(position);
+    },
+
+    getClosestPoints: function (position) {
+      // pull in points from model.getPoints
+
+      // First convert position lat + long from degrees to radians
+
+      // Next loop through array of points. 
+      // For each point's lat + long, convert degrees to radians,
+      // then plug all four values into Haversine formula to get
+      // distance from me to point.
+      // Add point to an array.
+
+      // After the loop completes, sort the array in ascending order
+      // of the new distance value.
+
+      // Return the first three items. 
+    
     }
 
   };
@@ -57,7 +94,7 @@
     },
 
     showLatLong: function(position) {
-      view.$results.html('Latitude: ' + position.coords.latitude +
+      this.$results.html('Latitude: ' + position.coords.latitude +
                     '<br /> Longitude: ' + position.coords.longitude);
     }
 
